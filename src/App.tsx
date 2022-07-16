9a4eda69df17f439fb6c2a5a777f2e7b4d8705ae
Abhinav Rajesh <abhinavrajesh@github.com>
2022-07-16 11:02:47 +0530

    added react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          {/* <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">This is the home page</h1>
+    </div>
+  );
+};
+
+export default Home;
