ee42387aa1e8e247a0dc461f5a23f29aab470fd0
Abhinav Rajesh <abhinavrajesh@github.com>
2022-07-17 13:22:22 +0530

    chore: fixed display of nfts

--- src/components/NFTCard/index.tsx
+++ src/components/NFTCard/index.tsx
@@ -13,7 +13,11 @@ const NFTCard = ({ image, description, postHashHex }: Props) => {
       className="flex items-start justify-start flex-col w-full"
     >
       <img src={image} className="w-full rounded" alt="" />
-      <p className="text-[14px] mt-[20px]">{description}</p>
+      <p className="text-[14px] mt-[20px]">
+        {description?.length > 150
+          ? `${description.slice(0, 147)}...`
+          : description}
+      </p>
       <span className="bg-primary text-black font-bold text-[14px] px-[20px] py-[5px] rounded mt-[20px]">
         View Post
       </span>
